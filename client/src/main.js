a8aab9aa618cca4cf77a26458dbbdeb942a9e8f9
Niek Vlam <vlam4695@gmail.com>
2019-12-06 15:03:02 +0100

    Init commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,25 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import VueToasted from "vue-toasted";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueToasted from 'vue-toasted';
 
 Vue.config.productionTip = false;
 
 // Load Toast Plugin
 Vue.use(VueToasted, {
-  iconPack: "fontawesome"
+  iconPack: 'fontawesome'
 });
-
-// if ("serviceWorker" in navigator) {
-//   try {
-//     navigator.serviceWorker.register("sw.js");
-//     console.log("ServiceWorker Registered");
-//   } catch (error) {
-//     console.log("ServiceWorker Not Registered");
-//   }
-// }
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
